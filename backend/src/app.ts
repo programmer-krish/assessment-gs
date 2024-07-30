04e45b5690a7d60ab4ebb6dd6f56de5a7cbed5d6
programmer-krish <s18002156@ousl.lk>
2024-07-30 18:58:31 +0530

    minor fixes in routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use("/todos", todoRoutes);
+app.use("/api/todos", todoRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
